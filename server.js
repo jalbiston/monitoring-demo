01feb71946dca72e0b091a903bb6c5b12e4bfb15
jalbiston <jacob.albiston@gmail.com>
2021-09-28 12:26:40 -0600

    added student endpoint

--- server.js
+++ server.js
@@ -14,7 +14,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'))
     rollbar.info('html was monitored successfully!')
 })
+// student stuff
 
+const studentArr= []
+
+app.post('/api/students', (req, res) => {
+    const {name} =req.body
+    // const name=req.body.name
+    studentArr.push(name)
+
+    rollbar.log('Student Successfully added!')
+    res.status(200).send(studentArr)
+})
 
 const port = process.env.PORT || 5656
 
